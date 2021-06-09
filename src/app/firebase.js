74828c8e867d7e5d7accafd009b0cad7e99c334a
JoelUll <alu0100987584@ull.edu.es>
2021-06-09 20:07:47 +0100

    changes for login functions

--- src/app/firebase.js
+++ src/app/firebase.js
@@ -1,24 +1,45 @@
 function registrar() {
   var email = document.getElementById('inputemail').value;
   var password = document.getElementById('inputpass').value;
-
-  /*firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    console.log(errorCode);
-    console.log(errorMessage);
-
-  });*/
+  
   firebase.auth().createUserWithEmailAndPassword(email, password)
   .then((userCredential) => {
-    // Signed in
     var user = userCredential.user;
-    // ...
   })
   .catch((error) => {
     var errorCode = error.code;
     var errorMessage = error.message;
-    console.log(errorCode);
-    console.log(errorMessage);
   });
 }
+
+function ingreso() {
+  var emaillog = document.getElementById('maillog').value;
+  var passwordlog = document.getElementById('passlog').value;
+
+  firebase.auth().signInWithEmailAndPassword(emaillog, passwordlog)
+    .then((userCredential) => {
+      // Signed in
+      var user = userCredential.user;
+      // ...
+    })
+    .catch((error) => {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      console.log(errorCode);
+      console.log(errorMessage);
+  });
+}
+
+function observerUser() {
+  firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        console.log("worked fine!");
+        var uid = user.uid;
+        // ...
+      } else {
+        console.log("No user active detected!");
+      }
+  });
+}
+
+observerUser();
